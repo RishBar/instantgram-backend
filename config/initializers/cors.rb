daf9ea3b2180e191ee4d4a40b575788ce36d51d4
Rishav Baral <rishavbaral@Rishavs-MacBook-Air.local>
2021-09-19 18:00:46 -0700

    add new url for cors allow acces origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['INSTANTGRAM_FRONTEND_URL'] || 'http://localhost:3000'
+    origins 'http://instantgram6-frontend.herokuapp.com', ENV['INSTANTGRAM_FRONTEND_URL'], 'http://localhost:3000'
 
     resource '*',
       headers: :any,
